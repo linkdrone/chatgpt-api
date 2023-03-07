33c2424ae142738155f2800866263a4681d7ec01
linkdrone <fengwanqing2014@gmail.com>
2023-03-07 15:11:51 +0800

    feat: Add dialog

---
+++ demos/dialog.ts
@@ -0,0 +1,60 @@
+import dotenv from 'dotenv-safe'
+import enquirer from 'enquirer'
+import { oraPromise } from 'ora'
+
+import { ChatGPTAPI } from '../src'
+
+dotenv.config()
+
+/**
+ * Demo CLI for testing basic functionality.
+ *
+ * ```
+ * npx tsx demos/dialog.ts
+ * ```
+ */
+async function main() {
+  const api = new ChatGPTAPI({
+    apiKey: process.env.OPENAI_API_KEY,
+    debug: false
+  })
+
+  const Q = [
+    {
+      type: 'input',
+      name: 'question',
+      message: 'Q:'
+    }
+  ]
+
+  let parentMessageId = ''
+  while (true) {
+    let answers = await enquirer.prompt<{ question: string }>(Q)
+    const question = answers.question
+
+    // Reset
+    if (question.toLocaleLowerCase() == 'reset') {
+      parentMessageId = ''
+      continue
+    }
+
+    try {
+      const res = await oraPromise(
+        api.sendMessage(question, { parentMessageId }),
+        {
+          text: question
+        }
+      )
+      parentMessageId = res.id
+
+      console.log(res.text)
+    } catch (e) {
+      console.error('Failed:' + e.message)
+    }
+  }
+}
+
+main().catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
